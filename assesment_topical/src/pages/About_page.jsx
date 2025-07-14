06057b7e0cab6ab05cae30a63671f7dd70ea32b8
Thom920 <palapessythom@gmail.com>
2025-07-14 15:09:16 +0200

    filled unnecessary pages with lorem ipsum

---
+++ assesment_topical/src/pages/About_page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About_page() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis quod quam nesciunt aspernatur ut voluptates enim voluptate, ad magnam amet itaque voluptatum at odio ipsa, quis corporis quae. Assumenda, sunt?</p>
+    </div>
+  );
+}
---
+++ assesment_topical/src/pages/ToDo_page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ToDo_page() {
+  return (
+    <div>
+      <h1>ToDo</h1>
+    </div>
+  );
+}
