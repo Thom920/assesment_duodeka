210d85f53000e78a5313e5a95a69d3cc65cb8081
Thom920 <palapessythom@gmail.com>
2025-07-16 20:19:43 +0200

    spelling mistake corrected
    used the exact same colors as topical btw :)

--- assesment_topical/src/pages/Contact_page.jsx
+++ assesment_topical/src/pages/Contact_page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Contact_page() {
   return (
     <div className="page-content">
-      <h1>Contacr</h1>
+      <h1>Contact</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis quod quam nesciunt aspernatur ut voluptates enim voluptate, ad magnam amet itaque voluptatum at odio ipsa, quis corporis quae. Assumenda, sunt?</p>
     </div>
   );
